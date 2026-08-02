08f2c377271c1747161eeaec4b68e53ad6f38623
lakshya <lakshyamurali153@gmail.com>
2026-08-02 22:36:58 +0530

    first commit

---
+++ day1.py
@@ -0,0 +1,5 @@
+name = input("enter your name:")
+age=input("enter your age:")
+print(f"hello, {name}, you are {age} years old.")
+age = int(age)+5
+print(f"In 5 years, you will be {age} years old.")
