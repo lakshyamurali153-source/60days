e156acb80bf90956b19f151af2d91c4fc2dd2269
lakshya <lakshyamurali153@gmail.com>
2026-08-05 20:59:43 +0530

    add try block

--- day4.py
+++ day4.py
@@ -1,14 +1,17 @@
-x = int(input("Enter a number: "))
-y = int(input("Enter a number: "))
-ch=input("Enter an operation (+, -, *, /): ")
-if ch=='+':
-    print(x+y)  
-elif ch=='-':
-    print(x-y)  
-elif ch=='*':
-    print(x*y)  
-elif ch=='/':
-    if y==0:
-        print("Error: Division by zero is not allowed.")
+try:
+    x = float(input("Enter a number: "))
+    y = float(input("Enter a number: "))
+    ch = input("Enter an operation (+, -, *, /): ")
+    if ch == '+':
+        print(x + y)
+    elif ch == '-':
+        print(x - y)
+    elif ch == '*':
+        print(x * y)
+    elif ch == '/':
+        if y == 0:
+         print("Error: Division by zero is not allowed.")
     else:
         print(x/y)
+except ValueError:
+    print("Error: Invalid input. Please enter numeric values.")
