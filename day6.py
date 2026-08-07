6e492c55f3fae45e5f13c05682177bb2315fa6f0
lakshya <lakshyamurali153@gmail.com>
2026-08-07 18:04:10 +0530

    day6

---
+++ day6.py
@@ -0,0 +1,11 @@
+with open("fizzbuzz.txt", "w") as f:
+   x=int(input("Enter a number: "))
+   for i in range(1,x+1):
+     if i % 3 == 0 and i % 5 == 0:
+        f.write("fizzbuzz\n")
+     elif i % 3 == 0:
+        f.write("fizz\n")
+     elif i % 5 == 0:
+        f.write("buzz\n")
+     else:
+        f.write(f"{i}\n")
